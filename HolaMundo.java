ac0e7db35b5cb1989df9e991bba932eedaa7ded8
carlosmantilla <carlosmcot@gmail.com>
2024-02-20 22:12:35 -0500

    java

---
+++ HolaMundo.java
@@ -0,0 +1,21 @@
+class HolaMundo {
+    
+    public static void main(String[] args) {
+        int resultado = 44444;
+        System.out.print("Hola Mundo Juan");
+        System.out.println("Hola Mundo" + resultado);
+        byte numP16 = 16;
+        byte numP127 = -1;
+        System.out.println(numP16);
+        System.out.println(numP127);
+        char letra1 = 8734;
+        System.out.println(letra1);
+        String nada = "";
+        System.out.println(nada);
+        System.out.println(" hola juan ");
+        int a = 3;
+        int b = 4;
+        resultado = a | b;
+        System.out.println(resultado);
+    }
+}
